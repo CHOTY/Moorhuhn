a814930d38e2b1f4cfe15106771158b0d33c1ca8
TinyPista <lukas.pivka@atlas.sk>
2024-04-22 13:54:48 +0200

    Let y os

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -6,9 +6,16 @@ public class Movement : MonoBehaviour{
 
     public Vector3 targetPosition;
     public float rychlost;
+    public float maximum;
 
     void Update() {
-        Vector3 directionToMove = new Vector3(targetPosition.x - transform.position.x, 0f, 0f);
-        transform.Translate(directionToMove.normalized * rychlost * Time.deltaTime);
+        if (objectTransform.position.y >= maximum){
+            Vector3 directionToMove = new Vector3(targetPosition.x - transform.position.x, 0f, 0f);
+            transform.Translate(directionToMove.normalized * rychlost * Time.deltaTime);
+        }
+       else {
+            Vector3 directionToMove = new Vector3(targetPosition.x - transform.position.x, targetPosition.y - objectTransform.position.y, 0f);
+            transform.Translate(directionToMove.normalized * rychlost * Time.deltaTime);
+       }
     }
 }
