9a67b92b0dd2d1972a28c155a989349acfbc7cb9
Matej Chlustik <matejchlustik221@gmail.com>
2024-04-25 11:02:44 +0200

    Death zone fix

---
+++ Assets/Scripts/ChickenDespawn.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChickenDespawn : MonoBehaviour
+{
+    void Start() { }
+    void Update() { }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Chick")
+        {
+            Destroy(other.gameObject); 
+            Debug.Log("DESPAWN");
+        }
+        if (other.gameObject.tag == "Projectile")
+        {
+            Destroy(other.gameObject); 
+            Debug.Log("WORKS WITH BULLET");
+        }
+    }
+}
