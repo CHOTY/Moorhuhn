e2d19c6b4bafcc0fa36e2f196dea1faf16f71e5b
CHOTY <martinko.choty@gmail.com>
2024-05-16 01:05:51 +0200

    death_particles

--- Assets/Scripts/TargetScript.cs
+++ Assets/Scripts/TargetScript.cs
@@ -6,18 +6,22 @@ public class TargetScript : MonoBehaviour
 {
 
     public int scoreAmount = 10; 
+    public GameObject explosionPrefab;
     void Start() { }
     void Update() { }
     private void OnCollisionEnter(Collision other)
     {
         if (other.gameObject.tag == "Projectile")
         {
+            if (explosionPrefab) {
+	                Instantiate (explosionPrefab, transform.position, transform.rotation);
             if(gameObject.tag=="ChickS"){
+            
                 GameManager.gm.setSpecialSpawned(false);
             }
             Destroy(gameObject); 
             GameManager.gm.ChangeScore(scoreAmount);
         }
     }
-
+}
 }
